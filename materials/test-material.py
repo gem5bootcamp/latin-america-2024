b77f3a36ca833aee727585e8d3671b9015f827c8
Jason Lowe-Power <jason@lowepower.com>
2024-10-22 14:56:27 -0700

    materials: Add script to test materials
    Signed-off-by: Jason Lowe-Power <jason@lowepower.com>

---
+++ materials/test-material.py
@@ -0,0 +1,71 @@
+#! /usr/bin/env python3
+"""
+This script is used to test the materials.
+
+Every gem5 script should have a docs string that explains what the script does
+how to run it.
+
+You should format the "how to run it" with a `$` to indicate a shell command.
+After `$`, there should be one of the gem5 binaries available in the
+docker container for the codespace (e.g., `gem5`, `gem5-mesi`, `gem5-vega`).
+
+When running this script, it takes one parameter which is the path to the
+script it should test. It parses that script for a docstring and the `$ gem5`
+command. This script then runs the command and checks for either a 0 exit code
+or for the string found after the `$` in the docstring.
+"""
+
+import argparse
+from pathlib import Path
+import re
+import subprocess
+import sys
+
+def main():
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument("script", type=str)
+    arg_parser.add_argument("--verbose", action="store_true")
+
+    args = arg_parser.parse_args()
+
+    with open(args.script, "r") as f:
+        script = f.read()
+
+    docstring = script.split('"""')[1]
+
+    if "$ gem5" not in docstring:
+        print("Warning: No gem5 command found in docstring")
+        return 0
+
+    gem5_command = "gem5" + docstring.split("$ gem5")[1].split("\n")[0].strip()
+    expected_output = docstring.split("$ gem5")[1].split("\n")[1].strip()
+    expected_output.replace("...", ".*")
+
+    print(f"Running: {gem5_command}")
+
+    # run the command and get the stdout
+    result = subprocess.run(gem5_command, shell=True, capture_output=True,
+                            text=True, cwd=Path(f"{args.script}").parent)
+
+    if result.returncode != 0:
+        print(f"Error: {result.returncode}")
+        print(result.stderr)
+        return 1
+
+    if not re.search(expected_output, result.stdout):
+        print("Error: output does not match expected output")
+        print(f"Command output: {result.stdout}")
+        print(f"Expected output: {expected_output}")
+        return 1
+
+    print("Success!")
+
+    if args.verbose:
+        print(result.stdout)
+        print(result.stderr)
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
