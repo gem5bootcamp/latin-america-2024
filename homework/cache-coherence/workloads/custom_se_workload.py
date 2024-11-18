407599e363a7cdc6a4737dd2178da3fc36a37aff
Zhantong Qiu <65193984+studyztp@users.noreply.github.com>
2024-11-18 18:23:02 +0000

    update the resource apis

--- homework/cache-coherence/workloads/custom_se_workload.py
+++ homework/cache-coherence/workloads/custom_se_workload.py
@@ -24,10 +24,10 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-from gem5.resources.workload import CustomWorkload
+from gem5.resources.resource import WorkloadResource
 
 
-class CustomSEWorkload(CustomWorkload):
+class CustomSEWorkload(WorkloadResource):
     def __init__(self, parameters: dict):
         super().__init__(
             function="set_se_binary_workload", parameters=parameters
